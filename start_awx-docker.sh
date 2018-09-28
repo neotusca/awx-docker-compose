fc3896bc6b74d7f0849f8c8927cf8561be948360
neotusca <neotusca@gmail.com>
2018-09-28 18:31:57 +0900

    script added

---
+++ start_awx-docker.sh
@@ -0,0 +1,8 @@
+
+
+
+/usr/local/bin/docker-compose -f docker-compose.yml   ps 
+/usr/local/bin/docker-compose -f docker-compose.yml   port 
+/usr/local/bin/docker-compose -f docker-compose.yml   logs 
+/usr/local/bin/docker-compose -f docker-compose.yml   top 
+
